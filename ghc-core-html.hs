96f7cb57a2933277903a570ec4adbae839561378
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2013-04-24 21:06:56 +0400

    Import ! unqualified. Mu8ch better for readabiliy

--- ghc-core-html.hs
+++ ghc-core-html.hs
@@ -13,6 +13,7 @@ import qualified Data.Foldable as F
 
 import Text.Blaze.Html5 (toHtml)
 import qualified Text.Blaze.Html5 as H
+import           Text.Blaze.Html5   ((!))
 import qualified Text.Blaze.Html5.Attributes as HA
 import Text.Blaze.Html.Renderer.Utf8
 
@@ -70,7 +71,7 @@ go opts (f:_) = do
               let table = allSyms xs
               LC.hPutStrLn stdout $ renderHtml $ onPage css js $ do
                   H.header $ do
-                      H.a H.! HA.id "buttonToggleBody" $ "toggle bodies"
+                      H.a ! HA.id "buttonToggleBody" $ "toggle bodies"
                       _ <- " - "
                       indexify table
                   F.foldMap (atomToHtml table) xs
@@ -81,8 +82,8 @@ go opts (f:_) = do
             H.head $ do
                 H.title "core-2-html"
                 H.style $ toHtml css
-                H.script H.! HA.src "http://code.jquery.com/jquery-1.9.1.min.js" $ ""
-                H.script H.! HA.type_ "text/javascript" $ toHtml js
+                H.script ! HA.src "http://code.jquery.com/jquery-1.9.1.min.js" $ ""
+                H.script ! HA.type_ "text/javascript" $ toHtml js
             H.body p
 
     allSyms = foldl i M.empty
@@ -94,7 +95,7 @@ go opts (f:_) = do
         --let allToplevelSyms = foldl i [] atoms
         let t = foldl prefixify pempty $ map fst $ M.toList table
         H.ul $
-            H.li H.! HA.class_ "idxdir" $ do
+            H.li ! HA.class_ "idxdir" $ do
                 treeToHtml "Index" t
         
       where prefixify a s =
@@ -106,18 +107,18 @@ go opts (f:_) = do
                 H.span $ toHtml n
                 H.ul $ do
                     forM_ (M.toList m) $ \(k,v) -> do
-                        H.li H.! HA.class_ "idxdir" $ do
+                        H.li ! HA.class_ "idxdir" $ do
                             treeToHtml k v
 
 
 atomToHtml :: M.Map String a -> Atom -> H.Html
 atomToHtml _ (Junk s) = H.section $
     H.pre (toHtml s)
-atomToHtml table (BindingP bind) = H.section H.! HA.class_ "binding" $ do
+atomToHtml table (BindingP bind) = H.section ! HA.class_ "binding" $ do
     anchor (bindSymbol bind)
     let (first:after) = lines $ bindBody bind
-    H.pre H.! HA.class_ "header" H.! HA.title (H.toValue $ signatureRaw $ bindSignature bind) $ colorify table first
-    H.pre H.! HA.class_ "body" $ colorify table $ unlines after
+    H.pre ! HA.class_ "header" ! HA.title (H.toValue $ signatureRaw $ bindSignature bind) $ colorify table first
+    H.pre ! HA.class_ "body" $ colorify table $ unlines after
 atomToHtml _ (RawBinding sym t errs) = H.section $ do
     anchor sym
     H.p (toHtml errs)
@@ -136,15 +137,15 @@ tokenToHtml table (Symbol s)  =
         found   = isJust $ M.lookup s table
         manchor = if found then toAnchor s else id
      in case mn of
-        Nothing -> manchor (H.span H.! HA.class_ "sym" $ toHtml s)
-        Just n  -> manchor (H.span H.! HA.class_ "sym" H.! HA.title (H.toValue s) $ toHtml n)
+        Nothing -> manchor (H.span ! HA.class_ "sym" $ toHtml s)
+        Just n  -> manchor (H.span ! HA.class_ "sym" ! HA.title (H.toValue s) $ toHtml n)
         
-tokenToHtml _ (Number n)  = H.span H.! HA.class_ "nu" H.! HA.title hexVal $ toHtml n
+tokenToHtml _ (Number n)  = H.span ! HA.class_ "nu" ! HA.title hexVal $ toHtml n
                               where hexVal = H.toValue ( printf "hex: 0x%x" (read n :: Integer) :: String)
 tokenToHtml _ (Spaces s)  = toHtml s
-tokenToHtml _ (StringT s) = H.span H.! HA.class_ "str" $ toHtml ("\"" ++ s ++ "\"")
-tokenToHtml _ (CharT c)   = H.span H.! HA.class_ "str" $ toHtml ("\'" ++ [c] ++ "\'")
-tokenToHtml _ (TypeDef s) = ":: " `mappend` (H.span H.! HA.class_ "ty" $ toHtml s)
+tokenToHtml _ (StringT s) = H.span ! HA.class_ "str" $ toHtml ("\"" ++ s ++ "\"")
+tokenToHtml _ (CharT c)   = H.span ! HA.class_ "str" $ toHtml ("\'" ++ [c] ++ "\'")
+tokenToHtml _ (TypeDef s) = ":: " `mappend` (H.span ! HA.class_ "ty" $ toHtml s)
 tokenToHtml _ Arrow       = "->"
 tokenToHtml _ Dot         = "."
 tokenToHtml _ BSlash      = "\\"
@@ -158,9 +159,9 @@ tokenToHtml _ RParen      = ")"
 tokenToHtml _ Unit        = "()"
 tokenToHtml _ LParenHash  = "(#"
 tokenToHtml _ RParenHash  = "#)"
-tokenToHtml _ Case        = H.span H.! HA.class_ "kw" $ "case"
-tokenToHtml _ Of          = H.span H.! HA.class_ "kw" $ "of"
-tokenToHtml _ Forall      = H.span H.! HA.class_ "kw" $ "forall"
+tokenToHtml _ Case        = H.span ! HA.class_ "kw" $ "case"
+tokenToHtml _ Of          = H.span ! HA.class_ "kw" $ "of"
+tokenToHtml _ Forall      = H.span ! HA.class_ "kw" $ "forall"
 tokenToHtml _ Underscore  = "_"
 tokenToHtml _ (Unknown s) = toHtml s
 
@@ -169,10 +170,10 @@ colorify :: M.Map String a -> String -> H.Html
 colorify table = F.foldMap (tokenToHtml table) . tokenify
 
 anchor :: H.ToValue a => a -> H.Html
-anchor sym = H.a H.! HA.name (H.toValue sym) $ H.span ""
+anchor sym = H.a ! HA.name (H.toValue sym) $ H.span ""
 
 toAnchor :: String -> H.Html -> H.Html
-toAnchor sym c = H.a H.! HA.href (H.toValue ('#' : sym)) $ c
+toAnchor sym c = H.a ! HA.href (H.toValue ('#' : sym)) $ c
 
 
 
